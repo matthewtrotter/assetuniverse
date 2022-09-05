162e6ce889b1063043b281fc18751723c6779fe8
semantic-release <semantic-release>
2022-09-05 09:27:40 -0500

    0.1.0
    Automatically generated by python-semantic-release

--- setup.py
+++ setup.py
@@ -1,8 +1,10 @@
 from setuptools import setup, find_packages
 
+__version__ = "0.1.0"
+
 setup(
     name="assetuniverse",
-    version="4.1.4",
+    version=__version__,
     description="Historical asset return downloader for multiple assets (an asset universe).",
     python_requires=">=3.6",
     install_requires=["yfinance", "pandas", "pandas_datareader", "numpy", "plotly", "pytest", "ib_insync", "appdirs", "arrow", "diskcache"],
